cbe6f2f155c6b85cf961d88c3f5ba16425b7ee79
Cracklos22 <132421709+Cracklos22@users.noreply.github.com>
2023-05-28 14:53:38 -0500

    Add files via upload

---
+++ BucleString.java
@@ -0,0 +1,12 @@
+public class BucleString {
+    public static void main(String[] args) {
+        String[] nombres = {"Carlos ", "Mauricio ", "Pardo"};
+        String ConcatenarNombres = "";
+        for (String nombre : nombres){
+        ConcatenarNombres += nombre;
+        System.out.println(ConcatenarNombres);
+        }
+
+
+    }
+}
